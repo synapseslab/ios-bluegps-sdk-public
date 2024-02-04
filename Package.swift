b29b4c62754129bfff925228dd486832dee1926b
valvoline <costantino@sofapps.it>
2024-02-04 12:25:35 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+
+import CompilerPluginSupport
+import PackageDescription
+
+let package = Package(
+    name: "bluegps-ios-sdk",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "bluegps-ios-sdk",
+            targets: ["bluegps-ios-sdk"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "bluegps-ios-sdk",
+            url: "https://github.com/synapseslab/bluegps-ios-sdk/xcframework.zip",
+            checksum: "21b0a62f0f7ce888b8a312169d534aa736261b42bf02d3fdde86aff0d816a5f4"
+        ),
+    ]
+)
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # bluegps-ios-sdk
+
 Official repository for the BlueGPS iOS SDK
