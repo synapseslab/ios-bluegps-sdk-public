836f7d980fb946c39bd9ee822d49e9f332820a75
valvoline <costantino@sofapps.it>
2024-02-08 10:31:45 +0100

    Update naming convention

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@ import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
-    name: "bluegps-ios-sdk",
+    name: "ios-bluegps-sdk-public",
     platforms: [
         .iOS(.v13),
     ],
     products: [
         .library(
-            name: "bluegps-ios-sdk",
-            targets: ["bluegps-ios-sdk"]),
+            name: "ios-bluegps-sdk-public",
+            targets: ["ios-bluegps-sdk-public"]),
     ],
     targets: [
         .binaryTarget(
-            name: "bluegps-ios-sdk",
+            name: "ios-bluegps-sdk-public",
             url: "https://github.com/synapseslab/bluegps-ios-sdk/raw/main/xcframework.zip",
             checksum: "b57510e603e75b14ae472099bcae308a5ebf4976c5efd6a15b2d62354c545ebb"
         ),
