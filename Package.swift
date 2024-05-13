53645fad24a4644738ed26b19c5587c9bc41ad6c
valvoline <costantino@sofapps.it>
2024-05-13 14:57:23 +0200

    Misc fix to SPM distribution

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ios-bluegps-sdk-public",
-            url: "https://github.com/synapseslab/bluegps-ios-sdk/raw/main/xcframework.zip",
+            url: "https://github.com/synapseslab/bluegps-ios-sdk/raw/1.0.90/xcframework.zip",
             checksum: "d77e7f16ad0c1d614799c2901d3adf3bb7769c27a90b3dd82dd0eacaf67b28aa"
         ),
     ]
