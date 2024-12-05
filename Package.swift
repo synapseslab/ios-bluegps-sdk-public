476f8af7de16a8a59c53cc8c74acfcea09c2087b
valvoline <costantino@sofapps.it>
2024-12-05 11:36:58 +0100

    Bump to build 1.0.99

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ios-bluegps-sdk-public",
-            url: "https://github.com/synapseslab/bluegps-ios-sdk/raw/1.0.98/xcframework.zip",
-            checksum: "6bec1c47eb8919785ecd8e370115be8ca1c176d50fafe697149c3c556962c7e0"
+            url: "https://github.com/synapseslab/bluegps-ios-sdk/raw/1.0.99/xcframework.zip",
+            checksum: "a041b99e6fddf64fa8783853f79e6f0a35cc7ebda02a87518d21c947b1563c01"
         ),
     ]
 )
