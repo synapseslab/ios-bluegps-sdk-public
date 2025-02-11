02eace5aefeaeb5bbb277657b37c88462fc648a6
valvoline <costantino@sofapps.it>
2025-02-11 17:00:05 +0100

    Code cleaning

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ios-bluegps-sdk-public",
-            url: "https://github.com/synapseslab/bluegps-ios-sdk/raw/1.0.106/xcframework.zip",
+            url: "https://github.com/synapseslab/bluegps-ios-sdk/raw/2.0.107/xcframework.zip",
             checksum: "c6d227327bdbf489a821ad1da996c2b18fe50659775666b6beab8b6a9fab949a"
         ),
     ]
